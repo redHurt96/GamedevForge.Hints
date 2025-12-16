16408404d4c419328aab7788da4eedaeced65e89
mikhailKolobov <michael.kolobof@gmail.com>
2025-12-16 19:16:56 +0300

    update folders

---
+++ Hints/Scripts/Hints.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GamedevForge.Hints
+{
+    public static class Hints
+    {
+        private static readonly HintsAdapter _adapter = new HintsAdapter();
+
+        public static void SetTarget(Transform target) => _adapter.SetTarget(target);
+
+        public static void Clear() => _adapter.Clear();
+    }
+}
